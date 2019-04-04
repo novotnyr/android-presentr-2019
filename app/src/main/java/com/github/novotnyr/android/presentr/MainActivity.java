51c6fb83f02eb99378159e14fc591d28984757d1
Robert Novotny <novotnr0@gmail.com>
2019-04-05 00:25:13 +0200

    Pridana ukazkova obsluha menu

--- app/src/main/java/com/github/novotnyr/android/presentr/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/presentr/MainActivity.java
@@ -2,6 +2,8 @@ package com.github.novotnyr.android.presentr;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_refresh) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
